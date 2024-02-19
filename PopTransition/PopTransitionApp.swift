21533de9d8fc65be6a1caec9932cefc1f63286ab
bullinnyc <i8u11@icloud.com>
2024-02-19 20:24:21 +0700

    Initial Commit

---
+++ PopTransition/PopTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopTransitionApp.swift
+//  PopTransition
+//
+//  Created by Dmitry Kononchuk on 19.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PopTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
